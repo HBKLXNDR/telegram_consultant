eb382663d6188554dd4cb0faee7454a87802fde2
HBKLXNDR <hbklksndr@protonmail.com>
2025-02-02 00:25:38 +0200

    Main functionality

---
+++ index.js
@@ -0,0 +1,29 @@
+// index.js
+const App = require('./app');
+const logger = require('./logger');
+
+process.on('uncaughtException', (error) => {
+    logger.error('Uncaught Exception:', {
+        error: error.message,
+        stack: error.stack
+    });
+    process.exit(1);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+    logger.error('Unhandled Rejection:', {
+        reason: reason,
+        promise: promise
+    });
+});
+
+try {
+    const app = new App();
+    app.start();
+} catch (error) {
+    logger.error('Failed to start application:', {
+        error: error.message,
+        stack: error.stack
+    });
+    process.exit(1);
+}
